cd47f8372277dcea0e996584593c9726bf6b3398
dineshDevAI <dineshsonawane06565@gmail.com>
2026-09-08 16:09:17 +0530

    Update problem5.java

---
+++ java basics/src/Arrays/array/problem5.java
@@ -0,0 +1,12 @@
+//print element using index
+package src.Arrays.array;
+
+public class problem5 {
+    public static void  main(String args[]){
+        int a []={50,30,5,10,5,98};
+            System.out.println(" index  0 " +a[0]);
+            System.out.println(" index  1 " +a[1]);
+            System.out.println(" index  2 " +a[3]);
+        
+    }
+}
