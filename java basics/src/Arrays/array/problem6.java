88eeea0495498d8dd1adbd01c26236692e8d39fe
dineshDevAI <dineshsonawane06565@gmail.com>
2026-09-08 17:17:14 +0530

    Update problem6.java

--- java basics/src/Arrays/array/problem6.java
+++ java basics/src/Arrays/array/problem6.java
@@ -1,1 +1,18 @@
+//Find the Sum of array
 
+package src.Arrays.array;
+/**
+ * problem6
+ */
+public class problem6 {
+
+    public static  void main(String args[]){
+        int arr[]={10,20,30,40,50};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+        }
+        System.out.println(sum);
+       
+    }
+}
