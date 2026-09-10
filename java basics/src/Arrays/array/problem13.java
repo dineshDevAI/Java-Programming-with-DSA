b78fe8c076574c88229d8e740ff395ba4adfb9d1
dineshDevAI <dineshsonawane06565@gmail.com>
2026-09-10 22:22:37 +0530

    Create problem13.java

---
+++ java basics/src/Arrays/array/problem13.java
@@ -0,0 +1,19 @@
+//Duplicate element
+
+package src.Arrays.array;
+public class problem13{
+    public static void main(String[] args) {
+
+        int arr[] = {10, 20, 30, 20, 40, 10};
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] == arr[j]) {
+                    System.out.println("Duplicate element = " + arr[i]);
+                }
+            }
+        }
+    }
+}
